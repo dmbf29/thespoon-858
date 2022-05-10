dd94c846602efd4c57a5a6576dfaebda528aba33
Douglas Berkley <douglasmberkley@gmail.com>
2022-05-11 08:52:46 +0900

    Added style to index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  # CRUD
+  # verb '/path', to: 'controller#action', as: :prefix
+  # the prefix refers to the path only, not the http verb
+
+  # Read All
+  get '/restaurants', to: 'restaurants#index', as: :restaurants # (restaurants_path)
+
+  # Create
+  # We need a form page
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # A place to send the form info
+  post '/restaurants', to: 'restaurants#create'
+
+  # Read One
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+
+  # Update
+  # We need a form page
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # A place to send the form info
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Delete
+  delete '/restaurants/:id', to: 'restaurants#destroy'
 end
 
 # link_to restaurant_path # show page
